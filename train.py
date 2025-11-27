17fb1eb1b67bd2d1f1e10565b7667d908ab161d6
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2025-11-27 20:02:54 +0330

    Update train.py

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def train(net, train_loader, loss_dict, optimizer, scheduler, logger, epoch, met
     for b_idx, data_label in enumerate(progress_bar):
         t_data_1 = time.time()
         reset_metrics(metric_dict)
-        global_step = epoch * len(data_loader) + b_idx
+        global_step = epoch * len(train_loader) + b_idx
 
         t_net_0 = time.time()
         results = inference(net, data_label, use_aux)
