6e1802cd4b08608c8794ab91579e3c89fc1a05f0
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2025-11-27 17:20:05 +0330

    Update model.py

--- model/model.py
+++ model/model.py
@@ -2,22 +2,77 @@ import torch
 from model.backbone import resnet, mobilenet, squeezenet, VisionTransformer
 import numpy as np
 
-class conv_bn_relu(torch.nn.Module):
-    def __init__(self,in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1,bias=False):
+class conv_bn_relu(nn.Module):
+    def __init__(self, in_channels, out_channels, upsample=0):
         super(conv_bn_relu,self).__init__()
-        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, 
-            stride = stride, padding = padding, dilation = dilation,bias = bias)
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation = 1, bias = False)
         self.bn = torch.nn.BatchNorm2d(out_channels)
         self.relu = torch.nn.ReLU()
+        self.upsample = upsample
 
     def forward(self,x):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
+        if self.upsample:
+            x = F.interpolate(x, scale_factor=2, mode="bilinear", align_corners=True)
+        return x
+
+class SegmentationBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, n_upsamples=0):
+        super().__init__()
+
+        blocks = [conv_bn_relu(in_channels, out_channels, upsample=bool(n_upsamples))]
+
+        if n_upsamples > 1:
+            for _ in range(1, n_upsamples):
+                blocks.append(conv_bn_relu(out_channels, out_channels, upsample=True))
+
+        self.block = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        return self.block(x)
+
+class FPNBlock(nn.Module):
+    def __init__(self, pyramid_channels, skip_channels):
+        super().__init__()
+        self.skip_conv = nn.Conv2d(skip_channels, pyramid_channels, kernel_size=1)
+
+    def forward(self, x, skip=None):
+        x = F.interpolate(x, scale_factor=2, mode="nearest")
+        skip = self.skip_conv(skip)
+        x = x + skip
+        return x
+
+class SegmentationBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, n_upsamples=0):
+        super().__init__()
+
+        blocks = [conv_bn_relu(in_channels, out_channels, upsample=bool(n_upsamples))]
+
+        if n_upsamples > 1:
+            for _ in range(1, n_upsamples):
+                blocks.append(conv_bn_relu(out_channels, out_channels, upsample=True))
+
+        self.block = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        return self.block(x)
+
+class SegmentationHead(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=4):
+        super(SegmentationHead, self).__init__()
+
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        self.upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        x = self.upsampling(x)
         return x
 
 class parsingNet(torch.nn.Module):
-    def __init__(self, size=(288, 800), pretrained=True, backbone='50', cls_dim=(100, 52, 4)):
+    def __init__(self, size=(288, 800), pretrained=True, backbone='50', cls_dim=(100, 52, 4), use_aux=False):
         # cls_dim: (num_gridding, num_cls_per_lane, num_of_lanes)
 
         super(parsingNet, self).__init__()
@@ -64,16 +119,46 @@ class parsingNet(torch.nn.Module):
         )
 
         initialize_weights(self.cls_cat)
+        self.total_dim = np.prod(cls_dim)
+        self.p5 = conv_bn_relu(512, 128) if backbone in ['34','18'] else conv_bn_relu(2048, 128)
+        self.p4 = FPNBlock(128, 256) if backbone in ['34','18'] else FPNBlock(128, 1024)
+        self.p3 = FPNBlock(128, 128) if backbone in ['34','18'] else FPNBlock(128, 512)
+        self.p2 = FPNBlock(128, 64) if backbone in ['34','18'] else FPNBlock(128, 256)
+        self.smooth5 = SegmentationBlock(128, 128, n_upsamples=3)
+        self.smooth4 = SegmentationBlock(128, 128, n_upsamples=2)
+        self.smooth3 = SegmentationBlock(128, 128, n_upsamples=1)
+        self.smooth2 = SegmentationBlock(128, 128, n_upsamples=0)
 
+        # Final layers output : n, num_of_lanes+1, h, w
+        self.finallayer = SegmentationHead(128*4, cls_dim[-1]+1)
+
+        initialize_weights(self.p5, self.p4, self.p3, self.p2,
+                            self.smooth5, self.smooth4, self.smooth3, self.smooth2, self.finallayer)
     def forward(self, x):
         # n c h w - > n 2048 sh sw
         # -> n 2048
-        x4 = self.model(x)
+        c2, c3, c4, c5 = self.model(x)
+        if self.use_aux:
+            
+            p5 = self.p5(c5)
+            p4 = self.p4(p5, c4)
+            p3 = self.p3(p4, c3)
+            p2 = self.p2(p3, c2)
+            p5 = self.smooth5(p5)
+            p4 = self.smooth4(p4)
+            p3 = self.smooth3(p3)
+            p2 = self.smooth2(p2)
+            seg = self.finallayer(torch.cat([p5, p4, p3, p2], dim=1))
+        else:
+            seg=None
+
+        x4 = self.model(c5)
 
         fea = self.pool(x4).view(-1, 1800)
 
         group_cat = self.cls_cat(fea).view(-1, *self.cls_dim)
-
+        if self.use_aux:
+            return group_cat, seg
         return group_cat
 
 def initialize_weights(*models):
@@ -100,3 +185,4 @@ def real_init_weights(m):
                 real_init_weights(mini_m)
         else:
             print('unkonwn module', m)
+
